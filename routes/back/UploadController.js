9dbc43cba20b3c38fd6dab592945831a332cd769
lzjlvlzj <mxdyxzy@163.com>
2015-09-21 12:36:17 +0800

    分支 first

---
+++ routes/back/UploadController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by GeoStar on 2015/9/11.
+ */
+var express = require('express');
+var uploadController = express.Router();
+var path = require('path');
+var util = require('util');
+var BlgUtil = require('../../common/BlgUtil');
+
+
+uploadController.get("/img/test",function(req,res){
+    res.render("back/test");
+});
+/**
+ * 图片上传
+ * */
+uploadController.post("/img/upload",function(req,res){
+    var option = {};
+    option.path =  BlgUtil.getDateString();
+    option.callback=function(rt){
+        //res.json(rt);//这个不可以为什么? image.js eval()
+        res.send(JSON.stringify(rt));
+    };
+    BlgUtil.uploadImg(req,res,option);
+});
+
+module.exports = uploadController;
