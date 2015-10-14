76474f91fa8c8f50dae8de8b0e776446523b50e4
lzjlvlzj <mxdyxzy@163.com>
2015-10-14 16:40:53 +0800

    optimize menu

--- public/javascripts/system.js
+++ public/javascripts/system.js
@@ -6,7 +6,12 @@ System.init = function(){
     //判断终端
     var isMobile = util.browser.versions.mobile;
     //导航
-    Navigation.list(isMobile);
+    var obj =  $("#blog-navbar-ul").html();
+    console.log(obj);
+    if(!obj){
+        Navigation.list(isMobile);
+    }
+
 };
 $(document).ready(function(){
     //导航
